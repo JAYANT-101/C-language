6964e7bec9f48b9b336ecb624ec6aad1989a9295
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-04-12 21:51:32 +0530

    datatypes in c

---
+++ datatype.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a=5;
+    long int b=11111;
+    double c= 4.4444444;
+    float d=4.23;
+    // char e="A";
+    short f=3233;
+    printf("%lu",sizeof(int));
+    return 0;
+}
