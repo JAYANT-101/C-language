49914899a67a2c8bfb6f6929b67aa3984b358d93
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-04-12 21:26:54 +0530

    add.c use

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+ int a,b;
+ printf("Enter number a: ");
+ scanf("%d",&a);
+ printf("Enter number b: ");
+ scanf("%d",&b);
+ printf("a is %d and b is %d\n",a,b);
+ printf("Sum of a and b is %d",a+b);
+    return 0;
+}
