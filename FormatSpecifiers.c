8bd872702baa7e76cf9e2c4164c3938a7f63ab44
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-04-13 17:02:10 +0530

    c

---
+++ FormatSpecifiers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define Pi 3.14
+int main()
+{
+    int a=8;
+    const float b=6.3333;// will not change
+    printf("the value od a is %d and the value of b is %4f\n",a,b);
+    printf("%8.4f\n",b);
+    printf("%-8.4f\n",b);
+    printf("%f",Pi);
+    printf("jayatnt\tsinha\a");
+    return 0;
+}
