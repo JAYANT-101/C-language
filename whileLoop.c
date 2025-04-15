d23e77f15d4896862fef6f094d7560c49a6b582e
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-04-16 01:28:56 +0530

    c again

---
+++ whileLoop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    printf("Enter a number\n");
+    int a,i=0;
+    scanf("%d",a);
+    while (i<a)
+    {
+        printf("Chodu\n");
+        i=i+1;
+    }
+    
+    return 0;
+}
