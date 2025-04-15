ca7172c0a9d9c83cdf82d6e7abd4245d27c0c140
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-04-16 01:37:37 +0530

    c

---
+++ ForLoop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    printf("Enter a number\n");
+    int a,i;
+    scanf("%d",a);
+    for(i=0;i<=a,i++;)
+    {
+        printf("Burraaa");
+    }
+    return 0;
+}
