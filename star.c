73d2c9eebb101c0652294d0720fc79fe9c7d4e85
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-04-26 20:22:54 +0530

    c bro

---
+++ star.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(n;n>0;n--){
+        for(int j=0;j<n;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
