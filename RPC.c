8bda72f005e9ffdc71b176396fc1d2ec9715a3f6
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-05-21 17:03:23 +0530

    c bro

---
+++ RPC.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    srand(time(NULL));
+    int c=(rand() %(3-1+1))+1;
+    printf("1:rock\n2:paper\n3:sisser\n");
+    int p;
+    scanf("%d",&p);
+    if(c==p){
+        printf("Tie\n");
+    }
+    else if (c==1&&p==3)
+    {
+        printf("loss\n");
+    }
+    else if (c==2&&p==1)
+    {
+        printf("loss\n");
+    }
+    else if (c==3&&p==2)
+    {
+        printf("loss\n");
+    }
+    else{
+        printf("win\n");
+    }
+    
+    
+    
+}
