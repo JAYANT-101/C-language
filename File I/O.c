02b02aa1a644e32c8adc21d0877364136daaf322
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-05-28 20:33:33 +0530

    c bro

---
+++ File I/O.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    FILE *ptr=NULL;
+    char string[34]="fuck you";
+    // read
+    // ptr=fopen("my.txt","r");
+    // fscanf(ptr,"%s",string);
+    // printf("%s",string);
+    // write
+    ptr=fopen("my.txt","a");
+    fprintf(ptr,"%s",string);
+    return 0;
+}
